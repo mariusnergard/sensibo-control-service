a5a2436d0fd82114622e1c27aa2b43baa3c06557
Marius Nergård <marius.nergard@nexusgroup.com>
2019-08-26 18:23:53 +0200

    Added autoCoolDownMode

--- app.js
+++ app.js
@@ -44,9 +44,14 @@ const key = process.env.APIKEY;
 const nightTemp = process.env.NighttimeTargetTemp;
 const nightStart = process.env.NighttimeStart;
 const nightEnd = process.env.NighttimeEnd;
+const autoCoolDown = process.env.AutoCoolDown;
+const autoCoolDownNightTime = process.env.AutoCoolDownNightTime;
+const autoCoolDownTrigger = process.env.autoCoolDownNightTime;
+
 let isNight = false;
 let resumeTo = 22;
 let deviceId = false;
+let autoCoolDownOn = false;
 
 const TurnOff = (acState) => {
   return new Promise((resolve, reject) => {
@@ -68,6 +73,45 @@ const TurnOn = (acState) => {
     })
   })};
 
+const CoolModeOn = (acState) => {
+  return new Promise((resolve, reject) => {
+    console.log('!! Turning on cool mode !!');
+    const newState = {...acState, on: true, mode: 'cool'};
+    axios.put(`https://home.sensibo.com/api/v1/pods/${deviceId}/timer/?apiKey=${key}`, {
+      minutesFromNow: 1,
+      acState: newState,
+    })
+  })};
+
+const HeatModeOn = (acState) => {
+  return new Promise((resolve, reject) => {
+    console.log('!! Turning on heat mode !!');
+    const newState = {...acState, on: true, mode: 'heat'};
+    axios.put(`https://home.sensibo.com/api/v1/pods/${deviceId}/timer/?apiKey=${key}`, {
+      minutesFromNow: 1,
+      acState: newState,
+    })
+  })};
+
+const AutoCoolModeOn = (acState) => {
+  return new Promise((resolve, reject) => {
+    console.log('!! Turning on cool mode !!');
+    const newState = {...acState, on: true, mode: 'cool'};
+    axios.put(`https://home.sensibo.com/api/v1/pods/${deviceId}/timer/?apiKey=${key}`, {
+      minutesFromNow: 1,
+      acState: newState,
+    })
+  })};
+
+const AutoCoolModeOff = (acState) => {
+  return new Promise((resolve, reject) => {
+    console.log('!! Turning on cool mode !!');
+    const newState = {...acState, on: false, mode: 'heat'};
+    axios.put(`https://home.sensibo.com/api/v1/pods/${deviceId}/timer/?apiKey=${key}`, {
+      minutesFromNow: 1,
+      acState: newState,
+    })
+  })};
 
 const service = () => {
   axios.get(`https://home.sensibo.com/api/v2/users/me/pods?fields=*&apiKey=${key}`, {
@@ -94,6 +138,32 @@ const service = () => {
       // Todo - Set fans speed based on diff!
       // Todo - Add logger
 
+      if (autoCoolDownOn) {
+        console.log('-- Currently in cool mode --');
+        // Check if we still want to cool down
+        if (roundTemp < targetTemperature + autoCoolDownTrigger) {
+          if (isNight && !autoCoolDownNightTime) {
+            // No cooltime at night
+            console.log('-- Do not cool down at nighttime --');
+            autoCoolDownOn = false;
+            await AutoCoolModeOff()
+              .catch(e => console.log(e));
+            return;
+          }
+          // Return if yes
+          console.log('-- Keep cooling down --');
+          return;
+        } else {
+          // Turn of cool mode if no
+          console.log('-- Temp below trigger, turn off cool mode --');
+          autoCoolDownOn = false;
+          await AutoCoolModeOff()
+            .catch(e => console.log(e));
+          return;
+        }
+
+      }
+
       if (!isNight) {
         console.log('-- Day time --');
         // Heat mode?
@@ -111,12 +181,39 @@ const service = () => {
             // Turn on ac
             await TurnOn()
               .catch(e => console.log(e));
+          } else if (roundTemp < targetTemperature + autoCoolDownTrigger && autoCoolDown) {
+            console.log('-- Temp above auto-cool-down trigger - change to cool mode --');
+            // Temp above auto-cool-down trigger
+            // Change to cool mode
+            autoCoolDownOn = true;
+            await AutoCoolModeOn()
+              .catch(e => console.log(e));
           } else {
             // AC State ok!
             console.log(`-- AC State ok! (On: ${acState.on}) --`);
           }
         } else {
           // Cool mode!
+          if (roundTemp < targetTemperature && on) {
+            console.log('-- Temp below target and ac is currently on --');
+            // Temp above target and ac is currently on
+            // Turn off ac
+            await TurnOff()
+              .catch(e => console.log(e));
+          } else if (roundTemp > targetTemperature && !on) {
+            console.log('-- Temp above target and ac currently off --');
+            // Temp below target and currently off
+            // Turn on ac
+            await TurnOn()
+              .catch(e => console.log(e));
+          } else if (roundTemp < targetTemperature + autoCoolDownTrigger && autoCoolDown) {
+            autoCoolDownOn = true;
+            await AutoCoolModeOn()
+              .catch(e => console.log(e));
+          } else {
+            // AC State ok!
+            console.log(`-- AC State ok! (On: ${acState.on}) --`);
+          }
         }
       } else {
         console.log('-- Night time --');
